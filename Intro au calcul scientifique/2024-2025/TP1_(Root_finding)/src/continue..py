4f5c21c3e200d290aa5ab2c4dc420b56a8cb5a90
Prestonne Mebou <prestonne.MebouKoagne+igthubcopilot@student.umons.ac.be>
2025-03-07 03:27:56 +0100

    Draft continue de root finding

--- Intro au calcul scientifique/2024-2025/TP1_(Root_finding)/src/continue..py
+++ Intro au calcul scientifique/2024-2025/TP1_(Root_finding)/src/continue..py
@@ -1,20 +1,41 @@
 # Description: Module pour les fonctions continues
-# Authors: Jonathan DEKHTIAR et Axel BRUYERE
-def g_b():
-    """
-        defini dans lénoncé du tp
-    """
-    pass
+# Authors: PRESTONNE MEBOU
+from typing import List
+from numpy import expm1
+from scipy.optimize import  newton
 
-def phi():
+def g_b(a:float,b:float)->float:
+    """
+        defini dans l'énoncé du tp
+    """
+    return b**2*(expm1(a) + a**3)-a**2 -4*a
+
+def dg_b(a:float,b:float)->float:
+    """
+        dérivée de g_b
+    """
+    return b**2*(expm1(a) + 3*a**2)-2*a -4
+
+def ddg_b(a:float,b:float)->float:
+    """
+        dérivée seconde de g_b
+    """
+    return b**2*(expm1(a) + 6*a)-2
+
+def phi(b:float)->float:
     """
         trouve le x* <0 unique racine de g_b(a) pour un b>=0
     """
-    pass
+    a_0 = 0
+    return newton(g_b,a_0,args=(b,))
 
 
-def continu(b:float)->list[float]:
+def continu(b:float)->List[float]:
     """
         ensemble des a pour lesquels f_ab est continue
     """
-    pass
+    a_star = phi(b)
+    if dg_b(a_star,b) > 0:
+        return [a_star]
+    else:
+        return [a_star - 1/(b**2)]
